67fe45f742c00500801bdb12b59a87d4c262dd60
rinaudo.ludmila <rinaudo.ludmila@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-02-06 22:21:43 +0000

    poc spring gwt

---
+++ trunk/src/src/com/gs/spring4/gwt/ex/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package com.gs.spring4.gwt.ex.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+* The async counterpart of GreetingService.
+*/
+public interface GreetingServiceAsync {
+ void greet(String input, AsyncCallback callback);
+}
