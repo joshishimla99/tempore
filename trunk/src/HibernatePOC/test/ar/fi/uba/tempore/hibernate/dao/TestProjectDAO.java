09134d7fdf1bd8a881ae2c5f0514e6cf9f4162e9
garcian@gmail.com <garcian@gmail.com@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-02-06 23:23:29 +0000

    Agregado y actualizacion de nuevas entidades del modelo

--- trunk/src/HibernatePOC/test/ar/fi/uba/tempore/hibernate/dao/TestProjectDAO.java
+++ trunk/src/HibernatePOC/test/ar/fi/uba/tempore/hibernate/dao/TestProjectDAO.java
@@ -2,6 +2,9 @@ package ar.fi.uba.tempore.hibernate.dao;
 
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.hibernate.Transaction;
 import org.hibernate.classic.Session;
@@ -10,7 +13,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import ar.fi.uba.tempore.hibernate.util.HibernateUtil;
+import fi.uba.tempore.poc.entities.Client;
 import fi.uba.tempore.poc.entities.Project;
+import fi.uba.tempore.poc.entities.ProjectState;
 import fi.uba.tempore.poc.entities.Task;
 
 public class TestProjectDAO {
@@ -32,7 +37,7 @@ public class TestProjectDAO {
 		transaction.commit();
 	}
 
-	@Test
+	//@Test
 	public void testFindById() {
 		p = pDAO.findById(1);
 		log.info("Objeto encontrado: " + p.getName() + ", " + p.getProjectState().getName());
@@ -41,21 +46,58 @@ public class TestProjectDAO {
 		for (Task t : p.getTaskList()){
 			log.info((i++)+") " + t.getName());
 		}
+		
+		log.info("Clientes del proyecto: ");
+		int j = 0;
+		for (Client t : p.getClientList()){
+			log.info(" \t"+(j++)+") " + t.getName());
+		}
 	}
 
-	@Test
-	public void testFindAll() {
-		fail("Not yet implemented"); // TODO
+	//@Test
+	public void testFindAll() {		
+		List<Project> findAll = pDAO.findAll();
+		int i=0;
+		for (Project pr : findAll){
+			log.info((i++) + ") "  + pr.getId() + ", " + pr.getName() + ", " + pr.getProjectState());
+		}
 	}
 
 	@Test
 	public void testMakePersistent() {
-		fail("Not yet implemented"); // TODO
+		p = new Project();
+		p.setId(null);
+		p.setName("Proyecto Test");		
+		
+		ProjectStateDAO  psDAO = new ProjectStateDAO();		
+		ProjectState ps = psDAO.findById(1);		
+		p.setProjectState(ps);
+		p.setTaskList(null);
+
+		ClientDAO cDAO = new ClientDAO();
+		Client cli =  cDAO.findById(1);
+		p.addClient(cli);
+		
+		p = pDAO.makePersistent(p);
+		log.info("Nuevo Proyecto Creado: ID = " + p.getId());		
+
+		cli.addProject(p);
+		cDAO.makePersistent(cli);		
+		log.info("Se actualiza cliente con proyecto: ID = " + cli.getId());
 	}
 
 	@Test
 	public void testDelete() {
-		fail("Not yet implemented"); // TODO
+		log.info("****** DELETE *********");
+		p = new Project();
+		p.setName("Proyecto Test");
+		
+		List<Project> findByExample = pDAO.findByExample(p);		
+		for (Project pr : findByExample){
+			log.info("Borrando proyecto ID = " + pr.getId() + " ("+pr.getName()+")");
+			pDAO.delete(pr);
+		}
+		log.info("**********************");
 	}
 
 }
