c0a3ba55e7d62b6516fcdf60a6e91ae59aa7aa0b
rinaudo.ludmila <rinaudo.ludmila@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-03-10 00:15:51 +0000

    rollback the home page. the popup is displayed when the user clicks on the login button

--- src/Spring4gwtEx/src/com/gs/spring4/gwt/ex/server/GreetingServiceImpl.java
+++ src/Spring4gwtEx/src/com/gs/spring4/gwt/ex/server/GreetingServiceImpl.java
@@ -1,19 +1,36 @@
 package com.gs.spring4.gwt.ex.server;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Service;
 
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.gs.spring4.gwt.ex.client.GreetingService;
+import com.gs.spring4.gwt.ex.client.users.User;
 
 /**
 * The server side implementation of the RPC service.
 */
 @SuppressWarnings("serial")
 @Service("greetingService")
-public class GreetingServiceImpl implements  GreetingService {
-
- public String checkLogin(String input, String password) {
+public class GreetingServiceImpl extends RemoteServiceServlet implements  GreetingService {
+ 
+ private static final String USER_SESSION = "GWTAppUser";
+	
+ public User checkLogin(String input, String password) {
 	 // TODO: Acceder al servicio que valide los datos en la base
-  return "Aca debemos validar a , " + input + " que tiene la clave " + password;
+	 User user = new User(input, password);
+//	 this.setUserInSession(user);
+     return user;
  }
-
+ 
+ private void setUserInSession(User user) {
+	    HttpSession session = getThreadLocalRequest().getSession();
+	    session.setAttribute(USER_SESSION, user);
+	}
+	 
+ public User getUserFromSession() {
+	    HttpSession session = getThreadLocalRequest().getSession();
+	    return (User) session.getAttribute(USER_SESSION);
+ }
 }
