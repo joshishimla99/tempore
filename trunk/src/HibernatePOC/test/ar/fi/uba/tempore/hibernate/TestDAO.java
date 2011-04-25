c93999fe1b8afb3830026f0c9b5c35990736e25b
garcian@gmail.com <garcian@gmail.com@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-04-25 02:51:47 +0000

    Inicio de pruebas con DBUnit

--- trunk/src/HibernatePOC/test/ar/fi/uba/tempore/hibernate/TestDAO.java
+++ trunk/src/HibernatePOC/test/ar/fi/uba/tempore/hibernate/TestDAO.java
@@ -1,6 +1,17 @@
 package ar.fi.uba.tempore.hibernate;
 
+import java.io.File;
+
 import org.apache.log4j.Logger;
+import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.ext.mysql.MySqlDataTypeFactory;
+import org.dbunit.operation.DatabaseOperation;
 import org.hibernate.Transaction;
 import org.hibernate.classic.Session;
 import org.junit.After;
@@ -8,19 +19,45 @@ import org.junit.Before;
 
 import ar.fi.uba.tempore.hibernate.util.HibernateUtil;
 
-public abstract class TestDAO {
+public class TestDAO extends DBTestCase{
 	protected final Logger log = Logger.getLogger(this.getClass());
 	private Transaction transaction;
+	private Session session = null;
 	
-	
+	public TestDAO(){		
+		super("DBUnit Demostracion....");
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+		
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.jdbc.Driver" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:mysql://localhost/tempore" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "root" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "" );
+	}
+		
+
 	@Before
-	public void setUp() throws Exception {
-		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-		transaction = session.beginTransaction();				
+	public void setUp() throws Exception {		
+		IDatabaseConnection connection = this.getConnection();
+		DatabaseConfig config = connection.getConfig();
+        config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MySqlDataTypeFactory());
+        
+		IDataSet dataSet = this.getDataSet();
+		DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
+		transaction = session.beginTransaction();
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		transaction.commit();
+	}
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		FlatXmlDataSet build = null;
+		File file = new File("./Test1_setup.xml");
+		if (file.isFile()){
+			build = new FlatXmlDataSetBuilder().build(file);
+		}
+		return build;
 	}
 }
