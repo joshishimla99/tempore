0ef96f45f2409d54ef8b1f5921403274b52f8daf
rinaudo.ludmila <rinaudo.ludmila@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-06-23 23:53:30 +0000

    project service

--- trunk/source/Tempore-GWT/src/ar/fi/uba/tempore/gwt/client/panel/ProjectPanel.java
+++ trunk/source/Tempore-GWT/src/ar/fi/uba/tempore/gwt/client/panel/ProjectPanel.java
@@ -1,11 +1,41 @@
 package ar.fi.uba.tempore.gwt.client.panel;
 
+import java.util.List;
+
+import ar.fi.uba.tempore.dto.ProjectDTO;
+import ar.fi.uba.tempore.gwt.client.ProjectServicesClient;
+import ar.fi.uba.tempore.gwt.client.ProjectServicesClientAsync;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.smartgwt.client.widgets.Label;
+import com.smartgwt.client.widgets.layout.HLayout;
 import com.smartgwt.client.widgets.layout.VLayout;
 
-public class ProjectPanel extends VLayout{
+public class ProjectPanel extends VLayout {
 
-	public ProjectPanel(){
-		this.addChild(new Label("Hola Mundo"));
+	List<ProjectDTO> projects;
+	private final ProjectServicesClientAsync projectService = GWT.create(ProjectServicesClient.class);
+
+	public ProjectPanel() {
+		final Label projectLabel = new Label();
+		this.addChild(projectLabel);
+		
+		projectService.getProjects(new AsyncCallback<List<ProjectDTO>>(){
+
+			@Override
+			public void onFailure(Throwable caught) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onSuccess(List<ProjectDTO> result) {
+				
+				projectLabel.setContents(result.get(0).getName());
+				projectLabel.draw();
+			}
+		});
 	}
+
 }
---
+++ trunk/source/Tempore-GWT/src/ar/fi/uba/tempore/gwt/server/ProjectServicesImpl.java
@@ -0,0 +1,22 @@
+package ar.fi.uba.tempore.gwt.server;
+
+import java.util.List;
+
+import ar.fi.uba.tempore.dto.ProjectDTO;
+import ar.fi.uba.tempore.service.ProjectServices;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class ProjectServicesImpl extends RemoteServiceServlet implements ar.fi.uba.tempore.gwt.client.ProjectServicesClient {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6786157718346471647L;
+
+	@Override
+	public List<ProjectDTO> getProjects() {
+			return new ProjectServices().allProject();
+	}
+
+}
