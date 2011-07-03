733f67b626824dca27218f3310604ff994bbd756
juanpablo.gigante@gmail.com <juanpablo.gigante@gmail.com@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-07-03 22:37:46 +0000

    updated para agregar el context panel y el service menu

--- trunk/source/Tempore-GWT/src/ar/fi/uba/tempore/gwt/client/panel/ContextPanel.java
+++ trunk/source/Tempore-GWT/src/ar/fi/uba/tempore/gwt/client/panel/ContextPanel.java
@@ -1,10 +1,16 @@
 package ar.fi.uba.tempore.gwt.client.panel;
 
+import com.smartgwt.client.widgets.Canvas;
+
 /*
  * Este panel es el contenedor de Ppal, Tarea, proyectos, etc.
  * 
  */
-public interface ContextPanel {
+public class ContextPanel extends Canvas{
 
+	public ContextPanel(){
+		this.addChild(new MainPanel());
+	}
+	
 	
 }
