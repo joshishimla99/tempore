8a64b9b625e5ee0c327661d07aae3809b2177bfd
juanpablo.gigante@gmail.com <juanpablo.gigante@gmail.com@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-07-03 22:35:38 +0000

    menu service para administrar las operaciones del menu

---
+++ trunk/source/Tempore-GWT/src/ar/fi/uba/tempore/gwt/client/MenuService.java
@@ -0,0 +1,30 @@
+package ar.fi.uba.tempore.gwt.client;
+
+import ar.fi.uba.tempore.gwt.client.panel.MiddlePanel;
+
+import com.google.gwt.user.client.ui.Widget;
+
+public class MenuService {
+	
+	private MiddlePanel middlePanel;
+	private static MenuService instance = null;
+	
+	private MenuService(){
+	}
+	
+	public static MenuService getInstance(){
+		if(instance == null) {
+			instance = new MenuService();
+		}
+		return instance;
+	}
+	
+	public void init(MiddlePanel middlePanel){
+		this.middlePanel = middlePanel;
+	}
+	
+	public void setNewContextPanel(Widget panel){
+		middlePanel.addMember(panel);
+	}
+
+}
