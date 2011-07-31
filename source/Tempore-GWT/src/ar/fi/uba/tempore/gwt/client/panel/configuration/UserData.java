5c4c9095f0e61a0e1693c4911f66cbfb380d1e00
rinaudo.ludmila <rinaudo.ludmila@gmail.com>
2011-07-31 20:57:59 +0000

    obtencion de datos

--- source/Tempore-GWT/src/ar/fi/uba/tempore/gwt/client/panel/configuration/UserData.java
+++ source/Tempore-GWT/src/ar/fi/uba/tempore/gwt/client/panel/configuration/UserData.java
@@ -1,29 +1,28 @@
 package ar.fi.uba.tempore.gwt.client.panel.configuration;
 
+import java.util.List;
 
-public class UserData {  
-  
-    private static UserRecord[] records;  
-  
-    public static UserRecord[] getRecords() {  
-        if (records == null) {  
-            records = getNewRecords();  
-        }  
-        return records;  
-    }  
-  
-    public static UserRecord[] getNewRecords() {  
-        return new UserRecord[]{  
-                new UserRecord("Rinaudo", "Ludmila Lis", "Gemalto", "1532070761", "ludmila.rinaudo@gemalto.com", "Argentina", "Eduardo Madero 900 - 21", "1980", "lrinaudo", "12345678", false,"Admin"),
-                new UserRecord("Gigante", "Juan Pablo", "Nobleza Picardo", "1532070760", "juanPablo.gigante@picardo.com", "Argentina", "Corrientes 900 - 21", "1980", "jgigante", "12345678", false,"Admin"),
-                new UserRecord("Garcia", "Nicolas", "EmpresaX", "1532070768", "nicolas.garcia@empresaX.com", "Argentina", "9 de Julio 900 - 21", "1980", "ngarcia", "12345678", false,"Admin"),
-                new UserRecord("Pantaleo", "Guillermo", "itMentor", "1532070754", "guillermo.pantaleo@itMentor.com", "Argentina", "Suipacha 900 - 21", "1980", "gpantaleo", "12345678", true,"Client"),
-                new UserRecord("Fernandez", "Ignacio", "Tata", "1532070767", "ignacio.fernandez@tata.com", "Argentina", "Jujuy 900 - 21", "1980", "ifernandez", "12345678", false,"User"),
-                new UserRecord("Perez", "Carlos", "Microsoft", "1532070723", "carlos.perez@microsoft.com", "Argentina", "Eduardo Madero 900 - 21", "1980", "cperez", "12345678", true,"Client"),
-                new UserRecord("Arnoni", "Mauro", "PetroleraX", "1532070756", "mauro.arnoni@petroleraX.com", "Argentina", "Independencia 900 - 21", "1980", "marnoni", "12345678", false,"User"),
-                new UserRecord("Riccillo", "Diego", "Gemalto", "1532070789", "diego.riccillo@gemalto.com", "Argentina", "Eduardo Madero 900 - 21", "1980", "driccillo", "12345678", false,"Admin"),
-                new UserRecord("Durante", "Maria", "Gemalto", "1532070761", "maria.durante@gemalto.com", "Argentina", "Eduardo Madero 900 - 21", "1980", "mdurante", "12345678", false,"User"),
-                new UserRecord("Lopez", "Julio", "Tata", "1534070761", "julio.lopez@gemalto.com", "Argentina", "Jujuy 900 - 21", "1980", "jlopez", "12345678", false,"Admin")
-        };  
-    }  
-}  
+import ar.fi.uba.tempore.dto.UserDTO;
+
+public class UserData {
+
+	private static UserRecord[] records;
+
+	public static UserRecord[] getRecords(List<UserDTO> userList) {
+		if (records == null) {
+			records = getNewRecords(userList);
+		}
+		return records;
+	}
+
+	public static UserRecord[] getNewRecords(List<UserDTO> userList) {
+		for (int i = 0; i < userList.size(); i++) {
+			records[i] = new UserRecord(userList.get(i).getLastName(), userList.get(i).getName(), 
+					userList.get(i).getCompany(), userList.get(i).getPhone(), userList.get(i).getEmail(), 
+					userList.get(i).getCountry(), userList.get(i).getAddress(),	userList.get(i).getZipCode(),
+					userList.get(i).getUserName(), userList.get(i).getPassword(), userList.get(i).isClient(),
+					userList.get(i).getRole());
+		}
+		return records;
+	}
+}
