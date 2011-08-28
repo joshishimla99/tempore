60d2b345a364666c02df8e2f5dbd821d4b201935
rinaudo.ludmila <rinaudo.ludmila@gmail.com>
2011-08-28 22:34:46 +0000

    new constant

--- source/Tempore/src/ar/fi/uba/tempore/gwt/client/panel/Constant.java
+++ source/Tempore/src/ar/fi/uba/tempore/gwt/client/panel/Constant.java
@@ -2,6 +2,9 @@ package ar.fi.uba.tempore.gwt.client.panel;
 
 public final class Constant {
 
+	/*
+	 * Menu options
+	 */
 	public static final int MENU_OPTION_MAIN = 0;
 	public static final int MENU_OPTION_NEW_PROJECT = 1;
 	public static final int MENU_OPTION_MODIFY_PROJECT = 2;
@@ -11,4 +14,18 @@ public final class Constant {
 	public static final int MENU_OPTION_USER_CONFIGURATION = 6;
 	public static final int MENU_OPTION_CLIENT_CONFIGURATION = 7;
 	public static final int MENU_OPTION_ALERT_CONFIGURATION = 8;
+	
+	/*
+	 * Icon Type for the project tree
+	 */
+	public static final int ICON_RESOURCE = 1;
+	public static final int ICON_TASK = 2;
+	public static final int ICON_PROJECT = 3;
+	
+	/**
+	 *  Generadores de Ids para el arbol de proyectos
+	 *  TODO: ver si hay alguna forma mejor de hacer esto
+	 */
+	public static final int ID_RESOURCE_GENERATOR = 1000;
+	public static final int ID_TASK_GENERATOR = 2000;
 }
