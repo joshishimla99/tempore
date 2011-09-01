64342a2e140cd5a8ded849bd61b0fae891705b74
rinaudo.ludmila <rinaudo.ludmila@gmail.com>
2011-09-01 00:07:13 +0000

    new package

---
+++ source/Tempore/src/ar/fi/uba/tempore/gwt/client/panel/menus/MainTabPanel.java
@@ -0,0 +1,57 @@
+package ar.fi.uba.tempore.gwt.client.panel.menus;
+
+import ar.fi.uba.tempore.gwt.client.panel.project.ProjectPanel;
+
+import com.smartgwt.client.widgets.tab.Tab;
+import com.smartgwt.client.widgets.tab.TabSet;
+import com.smartgwt.client.widgets.tab.events.TabSelectedEvent;
+import com.smartgwt.client.widgets.tab.events.TabSelectedHandler;
+
+public class MainTabPanel extends TabSet {
+	
+	public MainTabPanel(ProjectPanel projectPanel){
+		super();
+		super.setWidth(760);
+		super.setHeight(800);
+		
+		Tab timeTab = new Tab("Tempore  ", "../images/ico/schedule.ico");
+		timeTab.setWidth(100);
+		timeTab.setPane(new TimeTabPanel());
+		
+		Tab resourceTab = new Tab("Recursos  ", "../images/ico/user_group.ico");
+		resourceTab.setWidth(100);   
+		resourceTab.setPane(new ResourceTabPanel());
+		
+		Tab projectTab = new Tab("Proyectos  ", "../images/ico/briefcase.ico");
+		projectTab.setWidth(100);   
+		projectTab.setPane(new ProjectTabPanel(projectPanel));
+		
+		Tab taskTab = new Tab("Tareas  ", "../images/ico/notes.ico");
+		taskTab.setWidth(100);
+		taskTab.setPane(new TaskTabPanel(projectPanel));
+		
+		Tab reportTab = new Tab("Reportes  ", "../images/ico/report.ico");
+		reportTab.setWidth(100);
+		reportTab.setPane(new ReportTabPanel());
+		
+		Tab configurationTab = new Tab("Configuraci&oacute;n  ", "../images/ico/wrench.ico");
+		configurationTab.setWidth(100);
+		configurationTab.setPane(new ConfigurationTabPanel());
+		
+		Tab helpTab = new Tab("Ayuda  ", "../images/ico/help1.ico");
+		helpTab.setWidth(100);
+		helpTab.setPane(new HelpTabPanel());
+		
+		this.addTabSelectedHandler(new TabSelectedHandler() {
+            public void onTabSelected(TabSelectedEvent event) {
+                Tab selectedTab = event.getTab();
+                SubTabPanel subTabPanel = (SubTabPanel) selectedTab.getPane();
+                subTabPanel.loadChildPanels();
+            }
+        });
+
+		super.setTabs(timeTab, projectTab, resourceTab, taskTab, reportTab, configurationTab, helpTab);
+		super.draw();
+	}
+
+}
