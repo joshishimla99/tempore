57f5df7344322b02e2fe69d84e7d868a4b36e697
rinaudo.ludmila@gmail.com <rinaudo.ludmila@gmail.com@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-09-01 21:42:22 +0000

    nuevo tamanio

--- trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/client/panel/menus/ProjectTabPanel.java
+++ trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/client/panel/menus/ProjectTabPanel.java
@@ -1,29 +1,26 @@
 package ar.fi.uba.tempore.gwt.client.panel.menus;
 
-import com.smartgwt.client.widgets.tab.Tab;
-
-import ar.fi.uba.tempore.gwt.client.panel.project.ModifyProjectPanel;
 import ar.fi.uba.tempore.gwt.client.panel.project.NewProjectPanel;
 import ar.fi.uba.tempore.gwt.client.panel.project.ProjectPanel;
 
+import com.smartgwt.client.widgets.tab.Tab;
+
 public class ProjectTabPanel extends SubTabPanel{
 	
-	public ProjectTabPanel(ProjectPanel projectPanel){
+	private ProjectPanel projectPanel;
+	
+	public ProjectTabPanel(final ProjectPanel projectPanel){
 		super();
+		this.projectPanel = projectPanel;
 		
-		NewProjectPanel newProjectPanel = new NewProjectPanel(projectPanel);
-		ModifyProjectPanel modifyProjectPanel = new ModifyProjectPanel(projectPanel);
+		NewProjectPanel newProjectPanel = new NewProjectPanel(this.projectPanel);
 		
 		Tab newProjectTab = new Tab("Nuevo");
 		newProjectTab.setPane(newProjectPanel);
 		
-		Tab editProjectTab = new Tab("Editar");
-		editProjectTab.setPane(modifyProjectPanel);
-		
 		super.addChildPanels(newProjectPanel);
-		super.addChildPanels(modifyProjectPanel);
 		
-		super.getTabPanel().setTabs(newProjectTab, editProjectTab);
+		super.getTabPanel().setTabs(newProjectTab);
 		
 		super.getTabPanel().selectTab(0);
 		this.addChild(super.getTabPanel());
