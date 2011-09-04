b9953c03414a08a552387c065e841f24e6b5af1c
rinaudo.ludmila@gmail.com <rinaudo.ludmila@gmail.com@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-09-04 21:42:48 +0000

    se cambio el componente Vertical panel para que no entre en conflicto con los componentes de smartgwt

--- trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/client/panel/task/ModifyTaskPanel.java
+++ trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/client/panel/task/ModifyTaskPanel.java
@@ -2,23 +2,23 @@ package ar.fi.uba.tempore.gwt.client.panel.task;
 
 import ar.fi.uba.tempore.gwt.client.panel.menus.ContextChildPanel;
 
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.Label;
+import com.smartgwt.client.widgets.layout.VLayout;
 
 public class ModifyTaskPanel extends Canvas implements ContextChildPanel{
 
-	private VerticalPanel vPanel;
+	private VLayout vPanel;
 	
 	public ModifyTaskPanel(){
 		super();
-		this.vPanel = new VerticalPanel();
+		this.vPanel = new VLayout();
 	}
 
 	@Override
 	public void updateContent() {
 		// TODO Auto-generated method stub
-		this.vPanel.add(new Label("Modificar tareas"));
+		this.vPanel.addChild(new Label("Modificar tareas"));
 		this.addChild(this.vPanel);
 	}
 }
