9550dc6fdaaafda34f96561f94a63c03b1e69264
garcian@gmail.com <garcian@gmail.com@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-09-08 01:38:29 +0000

    fuentes viejos

--- trunk/src/HibernatePOC/src/ar/fi/uba/tempore/hibernate/dao/ProjectDAO.java
+++
@@ -1,8 +0,0 @@
-package ar.fi.uba.tempore.hibernate.dao;
-
-import fi.uba.tempore.poc.entities.Project;
-import ar.fi.uba.tempore.hibernate.util.GenericHibernateDAO;
-
-public class ProjectDAO extends GenericHibernateDAO<Project, Integer> {
-
-}
