7c665a84bd894c477704f1fe09088070b8cdc56e
rinaudo.ludmila@gmail.com <rinaudo.ludmila@gmail.com@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-09-09 18:57:37 +0000

    L&F login

--- trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/client/login/LoginPanel.java
+++ trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/client/login/LoginPanel.java
@@ -5,6 +5,9 @@ import ar.fi.uba.tempore.gwt.client.panel.ConteinerMainPanel;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.smartgwt.client.data.DataSource;
+import com.smartgwt.client.types.Alignment;
+import com.smartgwt.client.widgets.Img;
+import com.smartgwt.client.widgets.Label;
 import com.smartgwt.client.widgets.form.DynamicForm;
 import com.smartgwt.client.widgets.form.fields.ButtonItem;
 import com.smartgwt.client.widgets.form.fields.HeaderItem;
@@ -12,11 +15,12 @@ import com.smartgwt.client.widgets.form.fields.PasswordItem;
 import com.smartgwt.client.widgets.form.fields.TextItem;
 import com.smartgwt.client.widgets.form.fields.events.ClickEvent;
 import com.smartgwt.client.widgets.form.fields.events.ClickHandler;
+import com.smartgwt.client.widgets.layout.HLayout;
 import com.smartgwt.client.widgets.layout.VLayout;
 
 public class LoginPanel  extends Composite{
 
-	private VLayout layout = new VLayout(15);
+	private HLayout layout = new HLayout(15);
 	private DynamicForm formLogin = new DynamicForm();
 	private TextItem login;
 	private PasswordItem mdp ;
@@ -25,18 +29,19 @@ public class LoginPanel  extends Composite{
 	public LoginPanel(){
 		
 		DataSource dataSource = LoginDS.getInstance();
-		layout.setPageLeft(800);
+		layout.setPageLeft(240);
 		initWidget(layout);
 		formLogin.setShowEdges(true);
 		formLogin.setEdgeShowCenter(true);
 		formLogin.setEdgeImage("../images/login/glow_35.png");
 		formLogin.setEdgeSize(35);
-		formLogin.setEdgeOffset(25);
+		formLogin.setEdgeOffset(45);
 		formLogin.setDataSource(dataSource);
 		formLogin.setUseAllDataSourceFields(true);
-		
-		HeaderItem header = new HeaderItem();
-		header.setDefaultValue("Ingreso a Tempore");
+				
+		HeaderItem headerTitle = new HeaderItem();
+		headerTitle.setDefaultValue("Ingreso a Tempore");
+		headerTitle.setAlign(Alignment.CENTER);
 		
 		login = new TextItem();
 		login.setName("login");
@@ -46,10 +51,11 @@ public class LoginPanel  extends Composite{
 		mdp.setName("mdp");
 		mdp.setTitle("Clave");
 		mdp.setRequired(true);
-		mdp.setLength(20);
 		
 		ButtonItem validateItem = new ButtonItem();   
         validateItem.setTitle("Ingresar");   
+        validateItem.setWidth(75);
+        validateItem.setAlign(Alignment.CENTER);
         validateItem.addClickHandler(new ClickHandler() {   
             public void onClick(ClickEvent event) {   
                 if (formLogin.validate(false)){
@@ -57,7 +63,7 @@ public class LoginPanel  extends Composite{
                 	if (mdp.getValueAsString().equals("password") && login.getValueAsString().equals("user")){
                 		Window.alert("Validacion correcta");
                 		layout.clear();
-                		
+                		loadUser();
                 		ConteinerMainPanel.getInstance().init();
                 	} else{
                     	Window.alert("Validacion incorrecta" + " " + mdp.getValueAsString() +" " + login.getValueAsString() );
@@ -65,13 +71,78 @@ public class LoginPanel  extends Composite{
                     }
                 	
                 }
-            }   
+            }
+
         });
+		formLogin.setFields(headerTitle, login,mdp,validateItem);
 		
-		formLogin.setFields(header,login,mdp,validateItem);
+		/*
+		 * Imagenes y texto de la izquierda de la pagina del login
+		 * */
 		
+		Label space1 = new Label("");
+		
+		/*Quick
+		 * */
+		HLayout hLayoutQuick = new HLayout();
+		Label titleQuick = new Label("Rapida");
+		LoginImg  pictureQuick = new LoginImg ("Clock.png", "quick");
+		Label labelQuick = new Label("Tempore te permite cargar tus horas en forma rapida por medio de diversas formas de acceso al alcance de tu mano.");
+		labelQuick.setWidth(300);
+		hLayoutQuick.addMember(pictureQuick);
+		hLayoutQuick.addMember(space1);
+		hLayoutQuick.addMember(titleQuick);
+		hLayoutQuick.addMember(labelQuick);
+		/* Simple
+		 * */
+		Label space2 = new Label("");
+		HLayout hLayoutSimple = new HLayout();
+		Label titleSimple = new Label("Simple");
+		LoginImg pictureSimple = new LoginImg("Template.png", "simple");
+		Label labelSimple = new Label("Tempore es simple dado que brinda diferentes medios de acceso para que escogas el mas simple para ti.");
+		labelSimple.setWidth(300);
+		hLayoutSimple.addMember(pictureSimple);
+		hLayoutSimple.addMember(space2);
+		hLayoutSimple.addMember(titleSimple);
+		hLayoutSimple.addMember(labelSimple);
+		/*
+		 * */
+		Label space3 = new Label("");
+		HLayout hLayoutFriendly = new HLayout();
+		Label titleFriendly = new Label("Amigable");
+		LoginImg pictureFriendly = new LoginImg("Chat.png", "friednly");
+		Label labelFriendly = new Label("Tempore es simple dado que brinda diferentes medios de acceso para que escogas el mas simple para ti.");
+		labelFriendly.setWidth(300);
+		hLayoutFriendly.addMember(pictureFriendly);
+		hLayoutFriendly.addMember(space3);
+		hLayoutFriendly.addMember(titleFriendly);
+		hLayoutFriendly.addMember(labelFriendly);
+		
+		
+		VLayout vLayout = new VLayout();
+		vLayout.setWidth("600 px");
+		vLayout.addMember(hLayoutQuick);
+		vLayout.addMember(hLayoutSimple);
+		vLayout.addMember(hLayoutFriendly);
+		
+		layout.addMember(vLayout);
 		layout.addMember(formLogin);
 		
 		layout.redraw();
+	}
+	
+	private void loadUser() {
+		
+	}
+	
+	public static class LoginImg extends Img {
+		public LoginImg(String src, String name) {
+			setSrc(src);
+			setWidth(48);
+			setHeight(48);
+			setID(name);
+			setAppImgDir("../images/64x64/");
+		}
+
 	}
 }
