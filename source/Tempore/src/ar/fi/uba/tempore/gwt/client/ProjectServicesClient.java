6c47a8a5fba38fc00e529a2c6277d825d3f050cc
garcian <garcian@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-09-11 19:45:47 +0000

    Nuevo panel de Project

--- source/Tempore/src/ar/fi/uba/tempore/gwt/client/ProjectServicesClient.java
+++ source/Tempore/src/ar/fi/uba/tempore/gwt/client/ProjectServicesClient.java
@@ -2,12 +2,23 @@ package ar.fi.uba.tempore.gwt.client;
 import java.util.List;
 
 import ar.fi.uba.tempore.dto.ProjectDTO;
+import ar.fi.uba.temporeutils.listgrid.GenericGwtRpcDataSourceService;
 
-import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("ProjectServices")
-public interface ProjectServicesClient extends RemoteService {
+public interface ProjectServicesClient extends GenericGwtRpcDataSourceService<ProjectDTO> {
+
+	public static class Util{
+		private static ProjectServicesClientAsync instance = null;
+		public static ProjectServicesClientAsync getInstance(){
+			if (instance == null){
+				instance = GWT.create(ProjectServicesClient.class);
+			}
+			return instance;
+		}
+	}
 
 	List<ProjectDTO> getProjects() throws IllegalArgumentException;
 	
