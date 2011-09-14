1c696b1d95e5204e9e92f1620481d5130edc4b40
garcian <garcian@gmail.com>
2011-09-14 13:38:02 +0000

    Nueva implementacion de servicios

---
+++ source/Tempore/src/ar/fi/uba/tempore/gwt/server/TaskServicesImpl.java
@@ -0,0 +1,10 @@
+package ar.fi.uba.tempore.gwt.server;
+
+import ar.fi.uba.tempore.gwt.client.TaskServicesClient;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class TaskServicesImpl extends RemoteServiceServlet implements TaskServicesClient {
+
+	private static final long serialVersionUID = -2875888868382111997L;
+	
+}
