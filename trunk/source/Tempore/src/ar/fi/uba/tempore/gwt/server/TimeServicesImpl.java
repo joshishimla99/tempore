8a7689d3030c3cbd9f5d17a23a0efb4a5d27ac29
garcian@gmail.com <garcian@gmail.com@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-09-14 13:38:02 +0000

    Nueva implementacion de servicios

---
+++ trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/server/TimeServicesImpl.java
@@ -0,0 +1,11 @@
+package ar.fi.uba.tempore.gwt.server;
+
+import ar.fi.uba.tempore.gwt.client.TimeServicesClient;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class TimeServicesImpl extends RemoteServiceServlet implements TimeServicesClient {
+
+	private static final long serialVersionUID = -7296572794226418672L;
+
+	
+}
