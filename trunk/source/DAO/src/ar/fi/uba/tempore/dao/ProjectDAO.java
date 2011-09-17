ec2e0148b8aa5147c462c7590ad971a6fc27ab6a
garcian@gmail.com <garcian@gmail.com@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-09-17 17:36:45 +0000

    Se reescribe metodo para que guarde el estado

--- trunk/source/DAO/src/ar/fi/uba/tempore/dao/ProjectDAO.java
+++ trunk/source/DAO/src/ar/fi/uba/tempore/dao/ProjectDAO.java
@@ -2,7 +2,19 @@ package ar.fi.uba.tempore.dao;
 
 import ar.fi.uba.tempore.dao.util.GenericHibernateDAO;
 import ar.fi.uba.tempore.entity.Project;
+import ar.fi.uba.tempore.entity.ProjectState;
 
 public class ProjectDAO extends GenericHibernateDAO<Project, Integer> {
 
+	private ProjectStateDAO psDAO = new ProjectStateDAO();
+	
+	@Override
+	public Project makePersistent(Project entity) {
+		Integer stateId = entity.getProjectState().getId();
+		if (stateId != null){
+			ProjectState state = psDAO.findById(stateId);
+			entity.setProjectState(state);
+		}
+		return super.makePersistent(entity);
+	}
 }
