1f4e900690d96f94d22a7e59087342949484ad57
garcian@gmail.com <garcian@gmail.com@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-09-17 17:38:12 +0000

    Se agrega componente para persistencia de imagenes

--- trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/client/panel/help/HelpTabPanel.java
+++ trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/client/panel/help/HelpTabPanel.java
@@ -1,17 +1,32 @@
 package ar.fi.uba.tempore.gwt.client.panel.help;
 
+import ar.fi.uba.temporeutils.image.ImgClient;
+import ar.fi.uba.temporeutils.image.UpdateImgHandler;
+
+import com.google.gwt.user.client.Window;
 import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.Label;
 import com.smartgwt.client.widgets.layout.VLayout;
 
 public class HelpTabPanel extends Canvas {
+	private VLayout vLayout = new VLayout();
 	
 	public HelpTabPanel(){
 		super();
-		VLayout vLayout = new VLayout();
-		
-		vLayout.addMember(new Label("Creado por Juan Pablo Gigante, Ludmila Lis Rinaudo, Nicolas Garcia - Version 1.0"));
-		
-	}
+		Label titulo = new Label("GWT-Upload Demo");
 
+		final ImgClient img = new ImgClient();
+		img.setWidth(300);
+		img.setHeight(100);
+		img.addUpdateImageHandler(new UpdateImgHandler() {
+			@Override
+			public void onUpdatedImg(String fileName) {
+				Window.alert("Persistir nombre del archivo: " + fileName);
+			}
+		});
+		
+		vLayout.addMember(titulo);
+		vLayout.addMember(img);
+		this.addChild(vLayout);
+	}
 }
