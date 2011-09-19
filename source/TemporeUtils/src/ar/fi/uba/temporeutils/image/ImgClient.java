9f89b9d2d83123b5c7993d2a975239dcc6ed1247
garcian <garcian@gmail.com>
2011-09-19 10:36:26 +0000

    Actualizacion del componente para subir imagenes. Ahora esta compuesto por componentes smartGWT

--- source/TemporeUtils/src/ar/fi/uba/temporeutils/image/ImgClient.java
+++ source/TemporeUtils/src/ar/fi/uba/temporeutils/image/ImgClient.java
@@ -3,69 +3,80 @@ package ar.fi.uba.temporeutils.image;
 import java.util.ArrayList;
 import java.util.List;
 
-import gwtupload.client.IUploadStatus.Status;
-import gwtupload.client.IUploader;
-import gwtupload.client.IUploader.OnFinishUploaderHandler;
-import gwtupload.client.SingleUploader;
-
-import com.google.gwt.user.client.Window;
 import com.smartgwt.client.types.Alignment;
+import com.smartgwt.client.types.Encoding;
+import com.smartgwt.client.types.FormMethod;
+import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.Img;
+import com.smartgwt.client.widgets.form.DynamicForm;
+import com.smartgwt.client.widgets.form.fields.UploadItem;
+import com.smartgwt.client.widgets.form.fields.events.ChangeEvent;
+import com.smartgwt.client.widgets.form.fields.events.ChangeHandler;
 import com.smartgwt.client.widgets.layout.VLayout;
 
 public class ImgClient extends VLayout implements UpdateImgHandler {
 
 	private static final String DOWNLOAD_URL = "http://localhost:8080/Tempore/download.img?";
-	private static final String UPLOAD_URL = "http://localhost:8080/Tempore/upload.gupld?";
+	private static final String UPLOAD_URL = "uploadImage.upld?";
 	private static final String DEFAULT_IMAGE = "../images/unknownClient.jpg";
+	private static final String FRAME_RESPONSE = "frameResponse";
 	private List<UpdateImgHandler> listeners = new ArrayList<UpdateImgHandler>();
 	
-	final Img img = new Img(DEFAULT_IMAGE);
-	final SingleUploader link = new SingleUploader();
+	private final Img img = new Img(DEFAULT_IMAGE);
 	private String imgFileName;
 	
+	
 	public ImgClient (String clientId){
-		super(); 
+		addUploadJSNIListener(this);
 		
 		updateImage(clientId);
-
-		link.setHeight("20px");
-		link.setAutoSubmit(true);
-		link.addOnFinishUploadHandler(new OnFinishUploaderHandler() {
-			@Override
-			public void onFinish(IUploader uploader) {
-				if (uploader.getStatus() == Status.SUCCESS) {
-					String response = uploader.getServerInfo().message;
-					updateImage(response);
-					//Aviso que se actualizo el archivo
-					onUpdatedImg(response);
-				} else {
-					Window.alert("ERROR al subir archivo (STATUS = "+uploader.getStatus()+")");
-				}
-			}
-		});
 		
+//		img.setContents("<IFRAME name='"+FRAME_RESPONSE+"'></IFRAME>");
 		img.setHeight100();
 		img.setWidth100();
+		
+		final Canvas canvas = new Canvas();
+		
+		canvas.setWidth(10);
+		canvas.setHeight(10);
+		canvas.setContents("<IFRAME name='"+FRAME_RESPONSE+"' style='width:0;height:0;border:0'></IFRAME>");
+		
+		//Formulario
+		final DynamicForm form = new DynamicForm();
+		form.setEncoding(Encoding.MULTIPART);
+		form.setMethod(FormMethod.POST);
+		form.setAction(UPLOAD_URL);
+		form.setTarget(FRAME_RESPONSE);
+		
+		final UploadItem upload = new UploadItem("Imagen");
+		upload.addChangeHandler(new ChangeHandler() {
+			@Override
+			public void onChange(ChangeEvent event) {
+				form.submitForm();
+			}
+		});
+
+		form.setFields(upload);
 		
 		
 		this.setAlign(Alignment.CENTER);
 		this.setMembersMargin(10);
+		
 		this.addMember(img);
-		this.addMember(link);
+		this.addMember(canvas);
+		this.addMember(form);
 	}
 	
 	public ImgClient (){
 		this(null);
 	}
 
-	public void updateImage (String id) {
-		if (id != null && !id.isEmpty()){
+	public void updateImage (String uniqueFileName) {
+		if (uniqueFileName != null && !uniqueFileName.isEmpty()){
 			//La imagen ya existe
-			img.setSrc(DOWNLOAD_URL + "id=" + id + "&newurl=" + System.currentTimeMillis());
-			link.setServletPath(UPLOAD_URL + "id=" + id);
+			img.setSrc(DOWNLOAD_URL + "id=" + uniqueFileName + "&newurl=" + System.currentTimeMillis());
 		}
-		this.imgFileName = id;
+		this.imgFileName = uniqueFileName;
 	}
 
 	public String getImageFileName() {
@@ -82,8 +93,15 @@ public class ImgClient extends VLayout implements UpdateImgHandler {
 
 	@Override
 	public void onUpdatedImg(String fileName) {
-		for (UpdateImgHandler  listener : listeners) {
-			listener.onUpdatedImg(getImageFileName());
+		updateImage(fileName);
+		for (UpdateImgHandler listener : listeners) {
+			listener.onUpdatedImg(fileName);
 		}
 	}
+	
+	public native void addUploadJSNIListener(ImgClient x)/*-{
+		$wnd.uploadSuccess = function (param) {
+			x.@ar.fi.uba.temporeutils.image.ImgClient::onUpdatedImg(Ljava/lang/String;)(param)
+		};  
+	}-*/;
 }
