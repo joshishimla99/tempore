52c0fef1d42e36968e192280ae5cb1cdea56e6b2
garcian <garcian@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-09-27 02:22:50 +0000

    Usuario DTO

---
+++ trunk/source/DTO/src/ar/fi/uba/tempore/dto/UserProjectDTO.java
@@ -0,0 +1,42 @@
+package ar.fi.uba.tempore.dto;
+
+import java.io.Serializable;
+
+public class UserProjectDTO implements Serializable {
+
+	private static final long serialVersionUID = -5123217767892856110L;
+	private Integer id;
+	private Integer owner;
+
+//	private List<Alert> alertList = new ArrayList<Alert>();
+//	private List<Role> roleList = new ArrayList<Role>();
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getOwner() {
+		return owner;
+	}
+	public void setOwner(Integer owner) {
+		this.owner = owner;
+	}
+	
+//	public List<Alert> getAlertList() {
+//		return alertList;
+//	}
+//	public void setAlertList(List<Alert> alertList) {
+//		this.alertList = alertList;
+//	}
+//		
+//	public List<Role> getRoleList() {
+//		return roleList;
+//	}
+//	public void setRoleList(List<Role> roleList) {
+//		this.roleList = roleList;
+//	}
+	
+}
