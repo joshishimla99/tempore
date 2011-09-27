cac1fb7fb0e168ac6f19341a7addb6784388ecc6
garcian <garcian@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-09-27 02:22:26 +0000

    Nuevo metodo de inicializacion de BBDD

--- source/DAO/src/ar/fi/uba/tempore/dao/UserDAO.java
+++ source/DAO/src/ar/fi/uba/tempore/dao/UserDAO.java
@@ -1,8 +1,23 @@
 package ar.fi.uba.tempore.dao;
 
+import java.util.List;
+
 import ar.fi.uba.tempore.dao.util.GenericHibernateDAO;
 import ar.fi.uba.tempore.entity.User;
 
 public class UserDAO extends GenericHibernateDAO<User, Integer> {
+
+	public User validateUser(String userName, String password) {
+
+		User user = new User();
+		user.setUserName(userName);
+		user.setPassword(password);
+		List<User> findByExample = this.findByExample(user);
+		if (!findByExample.isEmpty()){
+			user = findByExample.get(0);
+		}
+		
+		return user;
+	}
 
 }
