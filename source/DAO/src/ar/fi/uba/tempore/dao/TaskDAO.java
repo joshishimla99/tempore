e5ab9b230484e19fd4dc1a4d6d46116ceb7d821a
rinaudo.ludmila <rinaudo.ludmila@gmail.com>
2011-09-28 01:43:53 +0000

    ahora el taskDAO getChildren no devuelve null, sino que devuelve una lista vacia

--- source/DAO/src/ar/fi/uba/tempore/dao/TaskDAO.java
+++ source/DAO/src/ar/fi/uba/tempore/dao/TaskDAO.java
@@ -1,5 +1,6 @@
 package ar.fi.uba.tempore.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ar.fi.uba.tempore.dao.util.GenericHibernateDAO;
@@ -9,7 +10,8 @@ public class TaskDAO extends GenericHibernateDAO<Task, Integer> {
 
 	public List<Task> getChildTask(Integer idTask) {
 		// TODO Auto-generated method stub
-		return null;
+		List<Task> taskList= new ArrayList<Task>();
+		return taskList;
 	}
 	
 }
