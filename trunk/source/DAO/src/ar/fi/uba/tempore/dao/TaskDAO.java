7869c3e909c4972e048b458a25653965fb56bba2
rinaudo.ludmila@gmail.com <rinaudo.ludmila@gmail.com@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-09-28 01:32:44 +0000

    modificaciones sobre el dao de las tareas

--- trunk/source/DAO/src/ar/fi/uba/tempore/dao/TaskDAO.java
+++ trunk/source/DAO/src/ar/fi/uba/tempore/dao/TaskDAO.java
@@ -1,8 +1,15 @@
 package ar.fi.uba.tempore.dao;
 
+import java.util.List;
+
 import ar.fi.uba.tempore.dao.util.GenericHibernateDAO;
 import ar.fi.uba.tempore.entity.Task;
 
 public class TaskDAO extends GenericHibernateDAO<Task, Integer> {
+
+	public List<Task> getChildTask(Integer idTask) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
