dc7f2186e972182b934db92eda9a83962ec90ec1
rinaudo.ludmila <rinaudo.ludmila@gmail.com>
2011-10-02 17:50:54 +0000

    se implemento el metodo para saber si una tarea tiene horas cargadas

--- source/DAO/src/ar/fi/uba/tempore/dao/TaskUserDAO.java
+++ source/DAO/src/ar/fi/uba/tempore/dao/TaskUserDAO.java
@@ -1,8 +1,26 @@
 package ar.fi.uba.tempore.dao;
 
+import java.util.List;
+
 import ar.fi.uba.tempore.dao.util.GenericHibernateDAO;
+import ar.fi.uba.tempore.entity.Task;
 import ar.fi.uba.tempore.entity.TaskUser;
 
 public class TaskUserDAO extends GenericHibernateDAO<TaskUser, Integer> {
+	
+	public int hoursChargedByTask(Integer idTask){
+		int chargedHour=0;
+		Task task = new Task();
+		task.setId(idTask);
+		TaskUser taskUser = new TaskUser();
+		taskUser.setTask(task);
+		List<TaskUser> findByExample = this.findByExample(taskUser);
+		if (!findByExample.isEmpty()){
+			for (TaskUser taskUserResult : findByExample) {
+				chargedHour += taskUserResult.getHourCount();
+			}
+		}
+		return chargedHour;
+	}
 
 }
