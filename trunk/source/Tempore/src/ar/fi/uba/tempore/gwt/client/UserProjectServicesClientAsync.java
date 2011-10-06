227498988798b49a15b8f5deed1871db7919868a
garcian <garcian@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-10-06 00:04:36 +0000

    Nuevos servicios

--- trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/client/UserProjectServicesClientAsync.java
+++ trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/client/UserProjectServicesClientAsync.java
@@ -1,8 +1,15 @@
 package ar.fi.uba.tempore.gwt.client;
 
-import ar.fi.uba.tempore.dto.UserProjectDTO;
-import ar.fi.uba.temporeutils.listgrid.GenericGwtRpcDataSourceServiceAsync;
+import java.util.List;
 
-public interface UserProjectServicesClientAsync extends GenericGwtRpcDataSourceServiceAsync<UserProjectDTO>{
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import ar.fi.uba.tempore.dto.UserDTO;
+import ar.fi.uba.tempore.dto.UserProjectDTO;
+import ar.fi.uba.temporeutils.listgrid.filter.GenericGwtRpcDataSourceServiceFilterIdAsync;
+
+public interface UserProjectServicesClientAsync extends GenericGwtRpcDataSourceServiceFilterIdAsync<Integer, UserProjectDTO>{
+
+	void getUserNotAssignedToProject(Integer projectId,AsyncCallback<List<UserDTO>> callback);
 
 }
