1a90941b25055398768342720973a9b94e2f09f7
garcian <garcian@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-10-30 00:54:21 +0000

    Actualizacion de los datos para el proyecto.  Habia un error antes

--- source/DAO/src/ar/fi/uba/tempore/dao/ProjectDAO.java
+++ source/DAO/src/ar/fi/uba/tempore/dao/ProjectDAO.java
@@ -1,5 +1,6 @@
 package ar.fi.uba.tempore.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Query;
@@ -9,19 +10,29 @@ import ar.fi.uba.tempore.entity.Project;
 
 public class ProjectDAO extends GenericHibernateDAO<Project, Integer> {
 	
-	public List<Project> getProjectsByUser (Integer userId){		
-		String hql = "select distinct p " +
-						"from Project as p " +
-						"inner join fetch p.userProjectList as up " +
-						"inner join fetch p.projectState as ps " +
-						//"inner join fetch p.client as c " +
-						"where up.user.id = " + userId;
+	@SuppressWarnings("unchecked")
+	public List<Project> getProjectsByUser (Integer userId){	
+		List<Project> list = null;
 		
-		Query createQuery = this.getSession().createQuery(hql);
-		
-		@SuppressWarnings("unchecked")
-		List<Project> list = createQuery.list();
+		if (userId != null){
+			String hql = "select distinct p " +
+							"from Project as p " +
+							"inner join fetch p.userProjectList as up " +
+							"inner join fetch p.projectState as ps " +
+							"inner join fetch p.client as c " +
+							"where up.user.id = " + userId;
+			
+			Query createQuery = this.getSession().createQuery(hql);
+			list = createQuery.list();
+		} else {
+			list = new ArrayList<Project>();
+		}
 		
 		return list;
+	}
+
+	private void list() {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- source/DAO/test/ar/fi/uba/tempore/hibernate/inicialize/InitGeneric.java
+++ source/DAO/test/ar/fi/uba/tempore/hibernate/inicialize/InitGeneric.java
@@ -42,7 +42,6 @@ public class InitGeneric extends DBTestCase{
         
 		IDataSet dataSet = this.getDataSet();
 		DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
-		//DatabaseOperation.DELETE_ALL.execute(connection, dataSet);
 		
 		transaction = HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
 		if (transaction.isActive()){
