c87723fe6e7f6ca9b887fbc826cf94a5b199775e
garcian <garcian@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-10-30 21:17:52 +0000

    Se agrega parte de filtro

--- trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/client/TimeServicesClient.java
+++ trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/client/TimeServicesClient.java
@@ -1,12 +1,13 @@
 package ar.fi.uba.tempore.gwt.client;
 import ar.fi.uba.tempore.dto.TaskUserDTO;
-import ar.fi.uba.temporeutils.listgrid.GenericGwtRpcDataSourceService;
+import ar.fi.uba.tempore.dto.TimeFilterDTO;
+import ar.fi.uba.temporeutils.listgrid.filter.GenericGwtRpcDataSourceServiceFilterId;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("TimeServicesClient")
-public interface TimeServicesClient extends GenericGwtRpcDataSourceService<TaskUserDTO>{
+public interface TimeServicesClient extends GenericGwtRpcDataSourceServiceFilterId<TimeFilterDTO, TaskUserDTO>{
 	/**
 	 * Utility class for simplifying access to the instance of async service.
 	 */
--- trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/client/TimeServicesClientAsync.java
+++ trunk/source/Tempore/src/ar/fi/uba/tempore/gwt/client/TimeServicesClientAsync.java
@@ -1,8 +1,9 @@
 package ar.fi.uba.tempore.gwt.client;
 
 import ar.fi.uba.tempore.dto.TaskUserDTO;
-import ar.fi.uba.temporeutils.listgrid.GenericGwtRpcDataSourceServiceAsync;
+import ar.fi.uba.tempore.dto.TimeFilterDTO;
+import ar.fi.uba.temporeutils.listgrid.filter.GenericGwtRpcDataSourceServiceFilterIdAsync;
 
-public interface TimeServicesClientAsync extends GenericGwtRpcDataSourceServiceAsync<TaskUserDTO>{
+public interface TimeServicesClientAsync extends GenericGwtRpcDataSourceServiceFilterIdAsync<TimeFilterDTO, TaskUserDTO>{
 
 }
