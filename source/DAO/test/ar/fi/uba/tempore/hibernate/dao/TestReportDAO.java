facb3c92a0c8d5ddbb207defa268025d29edd72d
garcian <garcian@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2011-11-20 22:41:42 +0000

    Creacion de nuevos reportes

--- source/DAO/test/ar/fi/uba/tempore/hibernate/dao/TestReportDAO.java
+++ source/DAO/test/ar/fi/uba/tempore/hibernate/dao/TestReportDAO.java
@@ -11,6 +11,9 @@ import org.junit.Test;
 
 import ar.fi.uba.tempore.dao.ReportDAO;
 import ar.fi.uba.tempore.entity.reports.ProjectsTimes;
+import ar.fi.uba.tempore.entity.reports.TasksTimes;
+import ar.fi.uba.tempore.entity.reports.TasksUsersTimes;
+import ar.fi.uba.tempore.entity.reports.UsersTimes;
 import ar.fi.uba.tempore.hibernate.TestDAO;
 
 public class TestReportDAO extends TestDAO{
@@ -18,7 +21,7 @@ public class TestReportDAO extends TestDAO{
 	
 	
 	@Test
-	public void testFindById() throws ParseException {
+	public void testProyectsTimes() throws ParseException {
 		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		Date ini = format.parse("2009-01-01");
 		Date end = new Date();
@@ -32,4 +35,66 @@ public class TestReportDAO extends TestDAO{
 		}
 	}
 	
+	@Test
+	public void testUserTimes() throws ParseException {
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date ini = format.parse("2009-01-01");
+		Date end = new Date();
+		
+		
+		List<UsersTimes> pTime = rDAO.getUsersTimes(ini, end);
+		
+		System.out.println("Cantidad de Registros: " + pTime.size());
+		for (UsersTimes r : pTime) {
+			System.out.println(r.getUserName() + " " + r.getUserLastName()+ ", Horas Cargadas: " + r.getHourCounted());
+		}
+	}
+	
+	@Test
+	public void testPrimaryTaskTimes() throws ParseException {
+		Integer projectId = 1;
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date ini = format.parse("2009-01-01");
+		Date end = new Date();
+		
+		
+		List<TasksTimes> pTime = rDAO.getPrimaryTaskTimes(projectId, ini, end);
+		
+		System.out.println("Cantidad de Registros: " + pTime.size());
+		for (TasksTimes r : pTime) {
+			System.out.println(r.getTaskName() + ", Horas Cargadas: " + r.getHourCounted());
+		}
+	}
+	
+	
+	@Test
+	public void testPrimaryTaskTimesXUsers() throws ParseException {
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date ini = format.parse("2009-01-01");
+		Date end = new Date();
+		
+		
+		List<TasksUsersTimes> pTime = rDAO.getPrimaryTaskTimesXUser(ini, end);
+		
+		System.out.println("Cantidad de Registros: " + pTime.size());
+		for (TasksUsersTimes r : pTime) {
+			System.out.println(r.getTaskName() + " - "+ r.getUserName() +" " + r.getUserLastName() + ", Horas Cargadas: " + r.getHourCounted());
+		}
+	}
+	
+	
+	@Test
+	public void testGetUserActivity() throws ParseException{
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date ini = format.parse("2009-01-01");
+		Date end = new Date();
+		Integer userId = 1;
+		
+		List<ProjectsTimes> userActivity = rDAO.getUserActivity(userId, ini, end);
+		
+		System.out.println("Cantidad de Registros: " + userActivity.size());
+		for (ProjectsTimes r : userActivity) {
+			System.out.println(r.getProjectName() + ", Horas Cargadas: " + r.getHourCounted());
+		}
+	}
 }
