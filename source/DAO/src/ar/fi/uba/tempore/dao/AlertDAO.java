95ccad8f666e00466fd87bde33be6eb46e5b0666
garcian <garcian@cfe252c5-be23-f3ad-80ab-7dd5dff73f43>
2012-01-22 02:19:20 +0000

    Bugs en las imagenes de las asignaciones.

--- source/DAO/src/ar/fi/uba/tempore/dao/AlertDAO.java
+++ source/DAO/src/ar/fi/uba/tempore/dao/AlertDAO.java
@@ -1,9 +1,21 @@
 package ar.fi.uba.tempore.dao;
 
+import org.hibernate.Query;
+
 import ar.fi.uba.tempore.dao.util.GenericHibernateDAO;
 import ar.fi.uba.tempore.entity.Alert;
 
 public class AlertDAO extends GenericHibernateDAO<Alert, Integer> {
+
+	public Integer deleteAllUserAlert(Integer projectUserId) {
+		String hql = "delete " +
+				" from Alert as a " +
+				" where a.userProject.id = :projectUserId"; 
+		
+		Query query = this.getSession().createQuery(hql).setInteger("projectUserId", projectUserId);
+		
+		return query.executeUpdate();
+	}
 
 	
 }
