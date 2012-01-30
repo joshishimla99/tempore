5595684c5182fca8dcd6b77cb97b893f5a2a5722
rinaudo.ludmila <rinaudo.ludmila@gmail.com>
2012-01-30 04:01:47 +0000

    L&F de reportes

--- source/Tempore/src/ar/fi/uba/tempore/gwt/client/panel/report/ReportTabPanel.java
+++ source/Tempore/src/ar/fi/uba/tempore/gwt/client/panel/report/ReportTabPanel.java
@@ -4,11 +4,11 @@ import java.util.Date;
 
 import ar.fi.uba.tempore.gwt.client.panel.TabsPanelContainer;
 
-import com.smartgwt.client.types.DragAppearance;
 import com.smartgwt.client.types.Overflow;
 import com.smartgwt.client.types.VisibilityMode;
 import com.smartgwt.client.util.SC;
-import com.smartgwt.client.widgets.Button;
+import com.smartgwt.client.widgets.HTMLFlow;
+import com.smartgwt.client.widgets.ImgButton;
 import com.smartgwt.client.widgets.Label;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
@@ -30,7 +30,8 @@ public class ReportTabPanel extends TabsPanelContainer{
      "<br><br><b>Horas cargadas por Usuario del Proyecto seleccionado</b><br>Reporte que representa por medio de l&iacute;neas la cantidad de horas cargadas por cada usuario del proyecto seleccionado." +  
      "<br><br><b>Horas cargadas a cada Tarea del Proyecto seleccionado</b><br>Reporte que grafica mediante &aacute;reas las horas cargadas a cada una de las tareas creadas para el proyecto seleccionado." +  
      "<br><br><b>Usuarios por proyecto</b><br>Reporte que grafica en columnas los usuario involucrados para todos los proyectos abiertos.";  
-	
+	 Integer IMAGE_SIZE = 62;
+	 
 	public ReportTabPanel() {
 		super();
 
@@ -38,71 +39,98 @@ public class ReportTabPanel extends TabsPanelContainer{
 		vLayout.setWidth100();
 		
 		SectionStack sectionStackReport = new SectionStack();
-		sectionStackReport.setDragAppearance(DragAppearance.TARGET);  
-		sectionStackReport.setOverflow(Overflow.HIDDEN);
-		sectionStackReport.setShowResizeBar(true);
-		sectionStackReport.setCanDragResize(true);  
-		sectionStackReport.setWidth(700);  
+		sectionStackReport.setWidth(800);  
 		sectionStackReport.setMaxHeight(500);
-		sectionStackReport.setCanReorderSections(true);
 		sectionStackReport.setVisibilityMode(VisibilityMode.MULTIPLE);  
 		
-		final Button btnReporte1 = new Button("Generar Reporte");
+		final ImgButton btnReporte1 = new ImgButton();
 		btnReporte1.addClickHandler(onClickReport1);
+		btnReporte1.setSrc("../images/report/LineReport.gif");
+		btnReporte1.setHeight(61);
+		btnReporte1.setWidth(61);
+		btnReporte1.setAltText("Generar reporte");
 		
-		final Button btnReporte2 = new Button("Generar Reporte");
+		final ImgButton btnReporte2 = new ImgButton();
 		btnReporte2.addClickHandler(onClickReport2);
+		btnReporte2.setSrc("../images/report/reportTorta.jpg");
+		btnReporte2.setHeight(54);
+		btnReporte2.setWidth(54);
+		btnReporte2.setAltText("Generar reporte");
 		
-		final Button btnReporte3 = new Button("Generar Reporte");
+		final ImgButton btnReporte3 = new ImgButton();
 		btnReporte3.addClickHandler(onClickReport3);
+		btnReporte3.setSize(IMAGE_SIZE);
+		btnReporte3.setSrc("../images/report/AreaReport.png");
+		btnReporte3.setHeight(55);
+		btnReporte3.setWidth(55);
+		btnReporte3.setAltText("Generar reporte");
 		
-		final Button btnReporte4 = new Button("Generar Reporte");
+		final ImgButton btnReporte4 = new ImgButton();
 		btnReporte4.addClickHandler(onClickReport4);
+		btnReporte4.setSrc("../images/report/ColumnReport.gif");
+		btnReporte4.setHeight(55);
+		btnReporte4.setWidth(55);
+		btnReporte4.setAltText("Generar reporte");
 		
-		SectionStackSection sectionReport1 = new SectionStackSection("Proyectos-Horas");  
-		sectionReport1.setExpanded(true);  
-		sectionReport1.setCanCollapse(true);
-		sectionReport1.setResizeable(false);
-		sectionReport1.setCanReorder(true);
-		sectionReport1.setItems(new Label("Reporte encargado de graficar en forma de torta la cantidad de horas totales reportadas para cada uno de los proyectos existentes en el sistema."), btnReporte1);
-        
+		HTMLFlow htmlFlow1 = new HTMLFlow();  
+	    htmlFlow1.setOverflow(Overflow.AUTO);  
+	    htmlFlow1.setPadding(10);
+	    
+	    HTMLFlow htmlFlow2 = new HTMLFlow();  
+	    htmlFlow2.setOverflow(Overflow.AUTO);  
+	    htmlFlow2.setPadding(10);
+	    
+	    HTMLFlow htmlFlow3 = new HTMLFlow();  
+	    htmlFlow3.setOverflow(Overflow.AUTO);  
+	    htmlFlow3.setPadding(10);
+	    
+	    HTMLFlow htmlFlow4 = new HTMLFlow();  
+	    htmlFlow4.setOverflow(Overflow.AUTO);  
+	    htmlFlow4.setPadding(10);
+		
+	    SectionStackSection sectionReport1 = new SectionStackSection("Proyectos-Horas");  
+	    sectionReport1.setExpanded(true);
+	    sectionReport1.setCanCollapse(true);
+		htmlFlow1.setContents("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Reporte encargado de graficar en forma de torta la cantidad de horas totales reportadas para cada uno de los proyectos existentes en el sistema.");
+		htmlFlow1.addChild(btnReporte1);
+		sectionReport1.addItem(htmlFlow1);
+		//sectionReport1.addItem(btnReporte1);
+		
 		SectionStackSection sectionReport2 = new SectionStackSection("Usuarios-Horas");  
 		sectionReport2.setExpanded(true);  
 		sectionReport2.setCanCollapse(true);
-		sectionReport2.setResizeable(false);
+		sectionReport2.setResizeable(true);
 		sectionReport2.setCanReorder(true);
-		sectionReport2.addItem(new Label("Reporte que representa por medio de l&iacute;neas la cantidad de horas cargadas por cada usuario del proyecto seleccionado."));
-		sectionReport2.addItem(btnReporte2);
+		htmlFlow2.setContents("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Reporte que representa por medio de l&iacute;neas la cantidad de horas cargadas por cada usuario del proyecto seleccionado.");
+		htmlFlow2.addChild(btnReporte2);
+		sectionReport2.addItem(htmlFlow2);
+		//sectionReport2.addItem(btnReporte2);
 		
 		SectionStackSection sectionReport3 = new SectionStackSection("Tareas-Horas");  
 		sectionReport3.setExpanded(true);  
 		sectionReport3.setCanCollapse(true);
 		sectionReport3.setResizeable(false);
 		sectionReport3.setCanReorder(true);
-		sectionReport3.addItem(new Label("Reporte que grafica mediante &aacute;reas las horas cargadas a cada una de las tareas creadas para el proyecto seleccionado."));
-		sectionReport3.addItem(btnReporte3);
+		htmlFlow3.setContents("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Reporte que grafica mediante &aacute;reas las horas cargadas a cada una de las tareas creadas para el proyecto seleccionado.");
+		htmlFlow3.addChild(btnReporte3);
+		sectionReport3.addItem(htmlFlow3);
+//		sectionReport3.addItem(btnReporte3);
 		
 		SectionStackSection sectionReport4 = new SectionStackSection("Usuario-Proyecto");  
 		sectionReport4.setExpanded(true);  
 		sectionReport4.setCanCollapse(true);
 		sectionReport4.setResizeable(false);
 		sectionReport4.setCanReorder(true);
-		sectionReport4.addItem(new Label("Reporte que grafica en columnas los usuario involucrados para todos los proyectos abiertos."));
-		sectionReport4.addItem(btnReporte4);
+		htmlFlow4.setContents("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Reporte que grafica en columnas los usuario involucrados para todos los proyectos abiertos.");
+		htmlFlow4.addChild(btnReporte4);
+		sectionReport4.addItem(htmlFlow4);
+		//sectionReport4.addItem(btnReporte4);
 		
-        sectionStackReport.addSection(sectionReport1);
+		sectionStackReport.addSection(sectionReport1);
         sectionStackReport.addSection(sectionReport2);
         sectionStackReport.addSection(sectionReport3);
         sectionStackReport.addSection(sectionReport4);
         
-//		final SelectItem selState = new SelectItem("Estado", "Tipo de Grafico");
-//		LinkedHashMap<String, String> valueMap = new LinkedHashMap<String, String>(4);  
-//		valueMap.put("0", "Torta");
-//		valueMap.put("1", "Linea");
-//		valueMap.put("2", "Areas");
-//		valueMap.put("3", "Columnas");
-//		selState.setValueMap(valueMap);
-		
         final Label title = new Label("Administraci&oacute;n de Reportes");
 		title.setWidth(200);
 		title.setHeight(15);
@@ -121,7 +149,6 @@ public class ReportTabPanel extends TabsPanelContainer{
 		final DateItem ini = new DateItem(DESDE_FIELD,"Desde");
 		ini.setValue(new Date(System.currentTimeMillis() + (3600000*24*30) ));
 		final DateItem end = new DateItem(HASTA_FIELD, "Hasta");
-		
 		
 		form.setFields(
 					ini,
