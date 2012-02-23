9b09301e05460429c5aaec55975f9e3d13a64f66
rinaudo.ludmila <rinaudo.ludmila@gmail.com>
2012-02-23 04:48:03 +0000

    se agrego la encripcion de la password

--- source/DAO/src/ar/fi/uba/tempore/dao/UserDAO.java
+++ source/DAO/src/ar/fi/uba/tempore/dao/UserDAO.java
@@ -3,9 +3,12 @@ package ar.fi.uba.tempore.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.naming.ServiceUnavailableException;
+
 import org.hibernate.Query;
 
 import ar.fi.uba.tempore.dao.util.GenericHibernateDAO;
+import ar.fi.uba.tempore.dao.util.PasswordServiceUtil;
 import ar.fi.uba.tempore.entity.User;
 
 public class UserDAO extends GenericHibernateDAO<User, Integer> {
@@ -14,37 +17,67 @@ public class UserDAO extends GenericHibernateDAO<User, Integer> {
 
 		User user = new User();
 		user.setUserName(userName);
-		user.setPassword(password);
-		List<User> findByExample = this.findByExample(user);
-		if (!findByExample.isEmpty()){
-			user = findByExample.get(0);
+		try {
+			
+			user.setPassword(hashPassword(password));
+			List<User> findByExample = this.findByExample(user);
+			if (!findByExample.isEmpty()) {
+				user = findByExample.get(0);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			return null;
 		}
-		
 		return user;
 	}
+
+	public String hashPassword(String password) throws ServiceUnavailableException{
+		return PasswordServiceUtil.getInstance().encrypt(password);
+	}
 	
-	
+	public User getUser(String userName) {
+		User user = new User();
+		user.setUserName(userName);
+		List<User> findByExample = this.findByExample(user);
+		if (!findByExample.isEmpty()) {
+			user = findByExample.get(0);
+		} else {
+			return null;
+		}
+		return user;
+	}
+
+	public boolean validateUserName(String userName) {
+		User user = new User();
+		user.setUserName(userName);
+		List<User> findByExample = this.findByExample(user);
+		if (!findByExample.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+
 	/**
 	 * Usuarios NO asignados al proyecto
-	 * @param projectId Id del proyecto
+	 * 
+	 * @param projectId
+	 *            Id del proyecto
 	 * @return Listado de usuarios
 	 */
 	@SuppressWarnings("unchecked")
-	public List<User> getUserNotAssignedToProject (Integer projectId){
+	public List<User> getUserNotAssignedToProject(Integer projectId) {
 		List<User> list = null;
-		if (projectId != null){
-			String hql = "select u" +
-					" from User u" +
-					" where u.id not in (select up.user.id " +
-					" from UserProject up " +
-					" where up.project.id = " + projectId + 
-					")";
+		if (projectId != null) {
+			String hql = "select u" + " from User u"
+					+ " where u.id not in (select up.user.id "
+					+ " from UserProject up " + " where up.project.id = "
+					+ projectId + ")";
 
 			Query createQuery = this.getSession().createQuery(hql);
 			list = createQuery.list();
 		} else {
 			list = new ArrayList<User>();
 		}
-		return list;		
+		return list;
 	}
 }
